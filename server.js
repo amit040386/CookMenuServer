c568aa83177d865beae5e0f67d81fc396ca434f4
amit040386 <amit7p.4u@gmail.com>
2016-02-28 23:25:55 +0530

    dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var config = require('./config/config'),
     express = require('./config/express');
 
 var app = express();
-app.listen(config.port);
+
+app.set('port', (process.env.PORT || config.port));
+
+app.listen(app.get('port'));
 
 module.exports = app;
-console.log('development server running at http://localhost:' + config.port);
+console.log('development server running at http://localhost:' + app.get('port'));
